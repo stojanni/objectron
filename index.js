f54a53e658499a535b0e0419b5897dbe7b88b231
Mihail <m.stojanni@gmail.com>
2023-08-12 22:42:05 +0300

    wefwe

--- index.js
+++ index.js
@@ -28,11 +28,6 @@ app.use(cors())
 app.use(express.json())
 app.use(express.static(path.join(__dirname, 'public')))
 
-app.use((req, res, next) => {
-    console.log(`${req.method} ${req.url}`)
-    next()
-})
-
 app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'public/index.html')))
 
 app.get('/platform', (req, res) => res.sendFile(path.join(__dirname, 'public/platform/index.html')))
