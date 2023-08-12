6f7b840557ed40d0439eac99933af62cbcd2e873
Mihail <m.stojanni@gmail.com>
2023-08-12 22:37:13 +0300

    asadsa

--- index.js
+++ index.js
@@ -26,9 +26,9 @@ const transporter = nodemailer.createTransport({
 const app = express()
 app.use(cors())
 app.use(express.json())
-app.use(express.static('./public'))
+app.use(express.static(path.join(__dirname, 'public')))
 
-app.get('/', (req, res) => res.sendFile('./public/index.html'))
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'public/index.html')))
 
 app.get('/platform', (req, res) => res.sendFile('./public/platform/index.html'))
 
